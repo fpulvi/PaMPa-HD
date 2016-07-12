0025b54c02a773bb0f391e4d8134dbfa6c66243a
fabiopulvi <fabiopulvirenti88@gmail.com>
2016-07-12 20:42:04 +0200

    Cleaning...

--- src/it/polito/dbdmg/pampa_HD/Driver.java
+++ src/it/polito/dbdmg/pampa_HD/Driver.java
@@ -72,22 +72,10 @@ public class Driver extends Configured implements Tool {
 	  tab_found=0;
 	  run_rid();
 	  numpath=2;
-	/*  if (tab_found==0) {
-		  System.out.println("\n No tables found, merging the results in an unique file\n");
-		  TempInput=TempOutput;
-		  TempOutput++;
-		  Input = new Path(this.FinalOutputDir+"/"+TempInput);
-		  Input2 = new Path(this.FinalOutputDir+"/"+(TempInput-1));
-		  Output = new Path(this.FinalOutputDir+"/FI");	
-		  numberOfReducers=1;
-		  run_rid();
-		  return 1;
-	  }*/
 	  while(1>0) {
 		  br_to_deep++;
 		  max_tables=max_tables*steps;
 		  if (max_tables<0) max_tables=2000000000;
-
 		  numpath=2;
 		  TempInput=TempOutput;
 		  TempOutput++;
@@ -96,12 +84,6 @@ public class Driver extends Configured implements Tool {
 		  System.out.println("\n Tables to expand found, starting another iteration\n");
 		  run_B();
 		  
-		 /* TempInput=TempOutput;
-		  TempOutput++;
-		  Input = new Path(this.FinalOutputDir+"/"+TempInput);
-		  Input2 = new Path(this.FinalOutputDir+"/"+(TempInput-1));
-		  Output = new Path(this.FinalOutputDir+"/"+TempOutput);		  
-		  run_rid(); */
 		  if (tab_found==0) break;
 	  }
 	  System.out.println("\n No tables found, merging the results in an unique file\n");
@@ -138,15 +120,14 @@ public class Driver extends Configured implements Tool {
 	    
 	    job.setInputFormatClass(TextInputFormat.class);
 	    
-	    job.setMapperClass(Pampa_A_Mapper.class);
-	    
+	    job.setMapperClass(Pampa_A_Mapper.class);	    
 	    
 	    job.setMapOutputKeyClass(Text.class);
 	    job.setMapOutputValueClass(Text.class);
 	    
 	    job.setReducerClass(Pampa_A_Reducer.class);
 	    
-	    //this is a comparator self written to force an arrival order to the reducer -- to do: measure impact
+	    //this is a comparator self written to force an arrival order to the reducer
 	    job.setSortComparatorClass(comparator_A.class);
 	    
 	    job.setNumReduceTasks(this.numberOfReducers);
@@ -162,7 +143,8 @@ public class Driver extends Configured implements Tool {
 	     
 	    return 0;}
 	    
-	    else {conf = job.getConfiguration();
+	    else {
+		conf = job.getConfiguration();
 	    	return 1;}
 	   // return job.waitForCompletion(true) ? 0 : 1;
   }
@@ -183,64 +165,46 @@ public class Driver extends Configured implements Tool {
   MultipleInputs.addInputPath(job, Input, TextInputFormat.class, Pampa_Sync_Mapper_1.class);
   
   if (numpath>1) {
-  	 MultipleInputs.addInputPath(job, Input2, TextInputFormat.class, Pampa_Sync_Mapper_2.class);}
-  	
+  	 MultipleInputs.addInputPath(job, Input2, TextInputFormat.class, Pampa_Sync_Mapper_2.class);}  
   
+  FileOutputFormat.setOutputPath(job, Output);  
   
-  FileOutputFormat.setOutputPath(job, Output);
-  
-  
-  job.setJarByClass(Driver.class);
-  
+  job.setJarByClass(Driver.class);  
   
   job.setMapOutputKeyClass(Text.class);
-  job.setMapOutputValueClass(Text.class);
-  
+  job.setMapOutputValueClass(Text.class);  
  
-  job.setReducerClass(Pampa_Sync_Reducer.class);
-  
-  
-  
+  job.setReducerClass(Pampa_Sync_Reducer.class);  
 
-  job.setNumReduceTasks(this.numberOfReducers);
-  
+  job.setNumReduceTasks(this.numberOfReducers);  
   
   job.setOutputKeyClass(Text.class);
-  job.setOutputValueClass(Text.class);
-  
+  job.setOutputValueClass(Text.class); 
   
  job.setOutputFormatClass(TextOutputFormat.class);
- 
 
-  
-  
-  //return job.waitForCompletion(true) ? 0 : 1;
  tab_found=0;
  long local_drop_count=0;
- long local_total_closed=0;
- 
+ long local_total_closed=0; 
  long local_kept_tables=0;
   if (job.waitForCompletion(true)==true)
-  {//conf = job.getConfiguration();
-   tab_found = job.getCounters().findCounter("found","found").getValue();
-   local_drop_count = job.getCounters().findCounter("drop_count_counter","drop_count_counter").getValue();
-   local_total_closed = job.getCounters().findCounter("total_closed_counter","total_closed_counter").getValue();
-  
-   local_kept_tables=  job.getCounters().findCounter("kept_tables","kept_tables").getValue();
-
-   System.out.println("\n on a total number of closed:"+local_total_closed+" has been dropped "+local_drop_count+" redundant values");
-
-   System.out.println("\n on a total number of sent tables: "+sent_tables_job_a+" has been kept "+local_kept_tables+" redundant tables");
-  double  ratio= (double) local_drop_count/(1+local_total_closed);
-  trend_of_pruning.add( ratio);
-   ratio= (double) (sent_tables_job_a-local_kept_tables)/(1+sent_tables_job_a);
-   //if (numberOfReducers!=1)
-   trend_of_pruning_tables.add (ratio);
-  return 0;}
-  
-  else {conf = job.getConfiguration();
-  	return 1;}
-  
+  {
+	   tab_found = job.getCounters().findCounter("found","found").getValue();
+	   local_drop_count = job.getCounters().findCounter("drop_count_counter","drop_count_counter").getValue();
+	   local_total_closed = job.getCounters().findCounter("total_closed_counter","total_closed_counter").getValue();	  
+	   local_kept_tables=  job.getCounters().findCounter("kept_tables","kept_tables").getValue();
+	   System.out.println("\n on a total number of closed:"+local_total_closed+" has been dropped "+local_drop_count+" redundant values");
+	   System.out.println("\n on a total number of sent tables: "+sent_tables_job_a+" has been kept "+local_kept_tables+" redundant tables");
+	  double  ratio= (double) local_drop_count/(1+local_total_closed);
+	  trend_of_pruning.add( ratio);
+	   ratio= (double) (sent_tables_job_a-local_kept_tables)/(1+sent_tables_job_a);
+	  
+	   trend_of_pruning_tables.add (ratio);
+	  return 0;}
+	  
+	  else {
+		conf = job.getConfiguration();
+	  	return 1;}  
   }
   
 
@@ -249,40 +213,30 @@ public class Driver extends Configured implements Tool {
 	  
 	    conf.set("minsup",Integer.toString(this.minsup));
 	    conf.set("br_to_deep", Integer.toString(this.br_to_deep));
-	    conf.set("max_tables",Integer.toString(this.max_tables));
-	  
+	    conf.set("max_tables",Integer.toString(this.max_tables));	  
 	   
 	    Job job = new Job(conf); 
 
-	    job.setJobName("PAMPA_B_minsup:"+Integer.toString(this.minsup)+"__"+Integer.toString(this.br_to_deep));
+	    job.setJobName("PAMPA_B_minsup:"+Integer.toString(this.minsup)+"__"+Integer.toString(this.br_to_deep));	   
+	    
+	    FileInputFormat.addInputPath(job, Input);	    
 	   
+	    FileOutputFormat.setOutputPath(job, Output);	    
 	    
-	    FileInputFormat.addInputPath(job, Input);
-	    
-	   
-	    FileOutputFormat.setOutputPath(job, Output);
-	    
-	    
-	    job.setJarByClass(Driver.class);
-	    
+	    job.setJarByClass(Driver.class);	    
 	    
 	    job.setInputFormatClass(TextInputFormat.class);
 	 
-	    job.setMapperClass(Pampa_B_Mapper.class);
-	    
+	    job.setMapperClass(Pampa_B_Mapper.class);	    
 	    
 	    job.setMapOutputKeyClass(Text.class);
 	    job.setMapOutputValueClass(Text.class);
-	    job.setReducerClass(Pampa_Sync_Reducer.class);
-	    
-	    
-	    
 
+	    job.setReducerClass(Pampa_Sync_Reducer.class);  
 	    job.setNumReduceTasks(this.numberOfReducers);
 	    
 	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(Text.class);
-	    
+	    job.setOutputValueClass(Text.class);	    
 	   
 	    job.setOutputFormatClass(TextOutputFormat.class);
 	    long local_drop_count;
@@ -291,23 +245,22 @@ public class Driver extends Configured implements Tool {
 	    long local_kept_tables;
 	    tab_found=0;
 	    if (job.waitForCompletion(true)==true)
-	    {//conf = job.getConfiguration();
-	  tab_found = job.getCounters().findCounter("found","found").getValue();
-	   local_drop_count = job.getCounters().findCounter("drop_count_counter","drop_count_counter").getValue();
-	   local_total_closed = job.getCounters().findCounter("total_closed_counter","total_closed_counter").getValue();
-	   local_total_sent_tables=  job.getCounters().findCounter("sent_tables","sent_tables").getValue();
-	   local_kept_tables=  job.getCounters().findCounter("kept_tables","kept_tables").getValue();
-
-	   System.out.println("\n on a total number of closed:"+local_total_closed+" has been dropped "+local_drop_count+" redundant values");
-
-	   System.out.println("\n on a total number of sent tables: "+local_total_sent_tables+" has been kept "+local_kept_tables+" redundant tables");
+	    {
+		   tab_found = job.getCounters().findCounter("found","found").getValue();
+		   local_drop_count = job.getCounters().findCounter("drop_count_counter","drop_count_counter").getValue();
+		   local_total_closed = job.getCounters().findCounter("total_closed_counter","total_closed_counter").getValue();
+		   local_total_sent_tables=  job.getCounters().findCounter("sent_tables","sent_tables").getValue();
+		   local_kept_tables=  job.getCounters().findCounter("kept_tables","kept_tables").getValue();
+		   System.out.println("\n on a total number of closed:"+local_total_closed+" has been dropped "+local_drop_count+" redundant values");
+		   System.out.println("\n on a total number of sent tables: "+local_total_sent_tables+" has been kept "+local_kept_tables+" redundant tables");
 	   double  ratio= (double) local_drop_count/(1+local_total_closed);
 	   trend_of_pruning.add( ratio);
 	   ratio= (double) (local_total_sent_tables-local_kept_tables)/(1+local_total_sent_tables);
 	   trend_of_pruning_tables.add (ratio);
-	    return 0;}
+	   return 0;}
 	    
-	    else {conf = job.getConfiguration();
+	    else {
+		conf = job.getConfiguration();
 	    	return 1;}
   }
   
@@ -321,8 +274,7 @@ public class Driver extends Configured implements Tool {
     this.minsup=Integer.parseInt(args[3]);
     this.max_tables=Integer.parseInt(args[4]);
     this.inputPath= new Path(args[1]);
-    this.steps= Integer.parseInt(args[5]);
-   
+    this.steps= Integer.parseInt(args[5]);   
     this.FinalOutputDir = new Path(args[2]);
   }
   
